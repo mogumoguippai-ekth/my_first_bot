7d2de92ee68dabebe67df3af4e42fc0f55673ad4
mogumoguippai-ekth <mogumogu.ippai@gmail.com>
2025-10-05 13:05:45 +0900

    修正しました3回目

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    app.run(host"0.0.0.0", port=port)  # どこからでもアクセスできる
+    app.run(host="0.0.0.0", port=port)  # どこからでもアクセスできる
